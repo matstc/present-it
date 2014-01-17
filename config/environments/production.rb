d27257194f1eb3136296fd88260755c8d2b649e3
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2014-01-17 23:59:41 +0100

    disables uglifier for js files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,10 @@ PresentIt::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -33,7 +33,7 @@ PresentIt::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.2'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
